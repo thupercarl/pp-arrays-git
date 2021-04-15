61638e174ddc05a1b83621e36d76c738fc2fc923
Jake Donaldson <Jdonaldson8@mail.greenriver.edu>
2021-04-15 12:32:50 -0400

    written index page

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Jake Donaldson
+4/15/21
+URL : https://jdonaldson.greenriverdev.com/328/pp-arrays-git/pp-arrays-git/index.php
+Pair Program 2
+Turn on error reporting
+*/
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Array Practice</title>
+</head>
+<body>
+
+<?php
+    echo "<p>PHP Array Practice</p>";
+
+
+?>
+
+</body>
+</html>
